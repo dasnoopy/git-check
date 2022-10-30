0b0c643afe1447f8aff2b350adc0b63f1bb3ea12
dasnoopy <a.antolini@tin.it>
2022-10-30 15:19:04 +0100

    add checktime and some info at beginning

--- git-check.py
+++ git-check.py
@@ -6,6 +6,7 @@ import sys
 import csv
 import subprocess
 import re
+import datetime
 
 gi.require_version("Gtk", "4.0")
 from gi.repository import Gtk
@@ -49,13 +50,17 @@ class colors:
 # pulizia screen e set dbname
 os.system('clear')
 # some variables
-fName='list_projects.txt'
+fName='list_projects.csv'
 verbose=True
 checkonly=False
 
 print ('Number of arguments:', len(sys.argv), 'arguments.')
 print ('Argument List:', str(sys.argv))
 print ()
+
+# initial checking  time
+adesso = datetime.datetime.now()
+now = adesso.strftime("%d/%m/%Y_%H:%M:%S")
 
 # open the file in read mode
 filename = open(fName, 'r')
@@ -65,36 +70,47 @@ dictName= csv.DictReader(filename)
 
 # creating empty lists
 repoList = []
+checktime =[]
 currentCommit = []
 
 # iterating over each row and append
 # values to empty list
 for col in dictName:
 	repoList.append(col['Repo_Name'])
+	checktime.append(col['Last_Check'])
 	currentCommit.append(col['Latest_Commit'])
 
 index = 0
 # set header for final list/multiline string to write in the text file
-all_rows  = ['Repo_Name,Latest_Commit'] 
+all_rows  = ['Repo_Name,Last_Check,Latest_Commit'] 
+
+# some statistic header
+print ('# Checking ' + str(len(repoList)) + ' remote git repos from file: ' + fName +'.')
+print ('# Current check time: ' + colors.fg.red + now)
+print (colors.reset)
 
 # check latest commit for each repo using git ls-remote commaand
 for repo_url in repoList:
 	process = subprocess.Popen(["git", "ls-remote", repo_url], stdout=subprocess.PIPE)
 	stdout, stderr = process.communicate()
 	lastCommit = re.split(r'\t+', stdout.decode('ascii'))[0]
-	print(colors.reset + "→ Checking : " + colors.fg.blue + colors.bold + repo_url)
+	print(colors.reset + '→ [' + str(index +1)+ '] git repo: ' + colors.fg.blue + colors.bold + repo_url)
+	
+	# update checking time
+	adesso = datetime.datetime.now()
+	now = adesso.strftime("%d/%m/%Y_%H:%M:%S")
 	
 	if currentCommit[index] != lastCommit:
-		print(colors.fg.red + '✔ ...git repo has been update since last check...')
+		print(colors.fg.red + '✔ ...changes since last check (' + checktime[index] + ')' )
 	else:
-		print(colors.fg.green + '✔ ...no changes from last check...')
+		print(colors.fg.green + '✔ ...no changes since last check (' + checktime[index] + ')' )
 	# show commits info
 	if verbose == True :
 		print(colors.reset + '→ Previous commit : ' + colors.fg.orange + currentCommit[index])
 		print(colors.reset + '→ Latest commit   : ' + colors.fg.lightcyan + lastCommit)
 
 	print (colors.reset)
-	all_rows.append(repo_url+','+lastCommit)
+	all_rows.append(repo_url+',' + now + ','+lastCommit)
 	index += 1
 
 filename.close()
@@ -109,7 +125,12 @@ if checkonly == False :
 		filename.close()
 
 # TODO list
-# parametro -v --verbose per stampare le tre righe
-# parametro -c --check-only per controllare ma non aggiornare file con ultimo commit
-# salvare e scrivere orario ultimo check  
+# parametro -v --verbose per stampare  righe commit
+# parametro -c --check-only per controllare ma non aggiornare il file con la lista dei repo, con ultimo commit
+# parametro -l --list usa una lista da un percorso e nome che si vuole es -l /home/andrea/dati/gitrep.txt
 # indentatura migliore usare main e def+
+#lista repo non viene trovata se si lancia script con path assoluto
+
+    # storing current date and time
+   # current_date_time = datetime.now()
+   #https://www.geeksforgeeks.org/how-to-add-timestamp-to-csv-file-in-python/
